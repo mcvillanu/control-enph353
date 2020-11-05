2ad038acbb33afbe58dc437cc1f0ae2647635d0c
mcvillanu <villanuevamichaelc@gmail.com>
2020-11-04 19:27:20 -0800

    not bad

--- node/pid.py
+++ node/pid.py
@@ -17,8 +17,10 @@ move = Twist()
 
 bridge = CvBridge()
 
-goal_position = 1150
+goal_position = 1100
 last_position = 1000
+call_one = True
+last_error = -1
 
 # size of the pictures is 720r x 1280c
 
@@ -28,7 +30,7 @@ def return_position(thresh):
     x_count = 0
     hit_white = False
 
-    for i in range(690,718):
+    for i in range(670,700):
         for j in reversed(range(600, 1279)):
             #print("row:", i, "column:",j, "value:", thresh[i][j])
             if(not hit_white and thresh[i][j] > 127):
@@ -45,9 +47,13 @@ def return_position(thresh):
     else:
         return last_position
 
+#def is_crosswalk():
+
+
 def imageCallback(data):
     global last_position
     global goal_position
+    global last_error
 
     try:
         kernel = np.ones((10,10),np.uint)
@@ -58,20 +64,38 @@ def imageCallback(data):
 
         opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
 
+
         # # print(thresh.shape)
         # cv2.imshow("frame",opening)
-        # cv2.waitKey(10)
+        cv2.waitKey(10)
 
         s_error = goal_position - return_position(opening)
 
         #print(s_error)
 
-        if(abs(s_error) > 60):
+        # if call_one:
+        #     for i in range(1,30000):
+        #         move.linear.x = 0.13
+        #         move.angular.z = 0.51
+        #         pub.publish(move)
+        #     call_one = False
+
+        if(abs(s_error) > 200):
+            move.linear.x = 0.0
+            move.angular.z = 0.8 * np.sign(s_error)
+        # elif(abs(s_error) > 60):
+        #     move.linear.x = 0.01
+        #     move.angular.z = 0.12 * np.sign(s_error)
+        # else:
+        #     move.linear.x = 0.15
+        #     move.angular.z = 0
+        elif(abs(s_error) > 30):
             move.linear.x = 0.01
-            move.angular.z = 0.153384 * np.sign(s_error)
+            move.angular.z = 0.12 * np.sign(s_error)
+            last_error = np.sign(s_error)
         else:
-            move.linear.x = 0.084339
-            move.angular.z = 0
+            move.linear.x = 0.13
+            move.angular.z = -last_error * 0.05
 
         pub.publish(move)
     except CvBridgeError, e:
